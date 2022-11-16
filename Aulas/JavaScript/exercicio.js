7bb9c81530a6ff60ee7357ef87e516545199c65c
NATHAN DA SILVA SANTOS <natan-silva-2011@hotmail.com>
2022-11-16 16:10:49 -0300

    Eventos em javascript

---
+++ Aulas/JavaScript/exercicio.js
@@ -0,0 +1,11 @@
+function clicou(){
+    document.querySelector("#div").innerText = "Clicou aqui"
+}
+
+function passouMouse(){
+    document.querySelector("#div").innerText = "O mouse está em cima de mim"
+}
+
+function tirouMouse(){
+    document.querySelector("#div").innerText = "Tirou o mouse"
+}
--- Aulas/JavaScript/script.js
+++ Aulas/JavaScript/script.js
@@ -1,8 +1,39 @@
-function clickMe(){
-    console.log("Fui clicado!")
+function clickMe(x){
+    x.style.background = "yellow"
+    console.log("O botão foi clicado!")
 }
 
-function hoverMe(){
-    console.log("Mouse aqui!")
+function overMe(x){
+    x.style.background = "yellow"
+    console.log("Mouse está aqui!")
 }
 
+function focusMe(x){
+    x.style.background = "yellow"
+    console.log("Campo focado!")
+}
+
+function blurMe(x){
+    x.style.background = "yellow"
+    console.log("Campo fora de foco!")
+}
+
+function onChange(x){
+    x.style.background = "yellow"
+    console.log("Valor alterado!")
+}
+
+function resetAll(){
+    console.log("Reset")
+    
+    for (i = 0; i < 5; i++){
+        x = document.getElementsByClassName("reset")[i]
+        x.style.background = "white"
+    }
+}
+
+function mudar(){
+    console.log("Clicado")
+    nome = document.querySelector("#exemplo").innerText = "Mudou"
+    
+}
