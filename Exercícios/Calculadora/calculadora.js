8f617d26367601c75d673d12fb06e3572df9f393
Nathan Santos <natan-silva-2011@hotmail.com>
2022-11-30 15:43:56 -0300

    Adicionado outras operações

--- Exercícios/Calculadora/calculadora.js
+++ Exercícios/Calculadora/calculadora.js
@@ -1,7 +1,27 @@
-function calcular(){
+function somar(){
     let n1 = document.querySelector("#n1").value
     let n2 = document.querySelector("#n2").value
     let resultado = document.querySelector("#resultado")
-
     resultado.innerHTML = parseInt(n1) + parseInt(n2)
+}
+
+function subtrair(){
+    let n1 = document.querySelector("#n1").value
+    let n2 = document.querySelector("#n2").value
+    let resultado = document.querySelector("#resultado")
+    resultado.innerHTML = parseInt(n1) - parseInt(n2)
+}
+
+function dividir(){
+    let n1 = document.querySelector("#n1").value
+    let n2 = document.querySelector("#n2").value
+    let resultado = document.querySelector("#resultado")
+    resultado.innerHTML = parseInt(n1) / parseInt(n2)
+}
+
+function multiplicar(){
+    let n1 = document.querySelector("#n1").value
+    let n2 = document.querySelector("#n2").value
+    let resultado = document.querySelector("#resultado")
+    resultado.innerHTML = parseInt(n1) * parseInt(n2)
 }
