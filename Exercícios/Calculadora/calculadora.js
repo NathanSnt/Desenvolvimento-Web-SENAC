0dc4c6f25737352c3ff1779243c3baa8c9b7807e
Nathan Santos <natan-silva-2011@hotmail.com>
2022-11-30 15:31:32 -0300

    nothing here

--- Exercícios/Calculadora/calculadora.js
+++ Exercícios/Calculadora/calculadora.js
@@ -1,7 +1,7 @@
-function calcular(){
-    let n1 = document.querySelector("#n1").value
-    let n2 = document.querySelector("#n2").value
-    let resultado = document.querySelector("#resultado")
+let n1 = document.querySelector("#n1").value
+let n2 = document.querySelector("#n2").value
+let resultado = document.querySelector("#resultado")
 
+function calcular(){
     resultado.innerHTML = parseInt(n1) + parseInt(n2)
 }
