ca25054e7174281d7f0f447ce438545ca0968b87
Nathan Santos <natan-silva-2011@hotmail.com>
2022-11-30 17:15:15 -0300

    Manipulação de classes e estilos com javascript

---
+++ Exercícios/ManipulaClasseComJS/script.js
@@ -0,0 +1,81 @@
+function fundoAzul(){
+    document.querySelector("#paragrafo").classList.add('fundoAzul')
+}
+
+function fundoTransparente(){
+    document.querySelector("#paragrafo").classList.remove('fundoAzul')
+}
+
+function fonteVermelha(){
+    document.querySelector("#paragrafo").classList.add('fonteVermelha')
+}
+
+function fonteNormal(){
+    document.querySelector("div").classList.remove('fonteVermelha')
+}
+
+function bordaVermelha(){
+    document.getElementById('paragrafo').style.borderColor = 'red'
+}
+
+function bordaAmarela(){
+    document.getElementById('paragrafo').style.borderColor = 'yellow'
+}
+
+function bordaVerde(){
+    document.getElementById('paragrafo').style.borderColor = 'green'
+}
+
+function bordaPreta(){
+    document.getElementById('paragrafo').style.borderColor = 'black'
+}
+
+function solid(){
+    document.getElementById('paragrafo').style.borderStyle = 'solid'
+}
+
+function dashed(){
+    document.getElementById('paragrafo').style.borderStyle = 'dashed'
+}
+
+function double(){
+    document.getElementById('paragrafo').style.borderStyle = 'double'
+}
+
+function dotted(){
+    document.getElementById('paragrafo').style.borderStyle = 'dotted'
+}
+
+function largura4(){
+    document.getElementById('paragrafo').style.borderWidth = '4px'
+}
+
+function largura6(){
+    document.getElementById('paragrafo').style.borderWidth = '6px'
+}
+
+function largura8(){
+    document.getElementById('paragrafo').style.borderWidth = '8px'
+}
+
+function largura10(){
+    document.getElementById('paragrafo').style.borderWidth = '10px'
+}
+
+function radius0(){
+    document.getElementById('paragrafo').style.borderRadius = '0px'
+}
+
+function radius5(){
+
+    document.getElementById('paragrafo').style.borderRadius = '5px'
+}
+
+function radius10(){
+
+    document.getElementById('paragrafo').style.borderRadius = '10px'
+}
+
+function radius15(){
+    document.getElementById('paragrafo').style.borderRadius = '15px'
+}
