5c4f1db21fec6aa949e34c396c5ece01203c93fe
Nathan Santos <natan-silva-2011@hotmail.com>
2022-11-30 16:34:45 -0300

    Criando elementos com javascript

--- Exercícios/Calculadora/calculadora.js
+++ Exercícios/Calculadora/calculadora.js
@@ -25,3 +25,17 @@ function multiplicar(){
     let resultado = document.querySelector("#resultado")
     resultado.innerHTML = parseInt(n1) * parseInt(n2)
 }
+
+let paragrafo = document.createElement('p')
+paragrafo.innerText = 'Segunda tem jogo'
+document.body.appendChild(paragrafo)
+
+function addFormat(){
+    // document.querySelector("#minhaDiv").classList.add('classe1', 'classe2', 'classe3')
+    document.querySelector("#minhaDiv").classList.add('classe1', 'classe3', 'classe2')
+}
+
+function remFormat(){
+    // document.querySelector("#minhaDiv").classList.add('classe1', 'classe2', 'classe3')
+    document.querySelector("#minhaDiv").classList.remove('classe1', 'classe3', 'classe2')
+}
