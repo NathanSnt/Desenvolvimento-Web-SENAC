b22247e95482f90a670b563fd3f064fb5bb841ed
Nathan Santos <natan-silva-2011@hotmail.com>
2023-01-27 13:46:35 -0300

    Adicionado mais um passo no passo a passo

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ let Nome: String = "Nathan"
 let Idade: number = 22
 
 console.log(`Meu nome é ${Nome} e eu tenho ${Idade} anos.`)
-console.log(`Nasci no ano ${2022-Idade}`)
+console.log(`Nasci no ano ${2022-Idade}.`)
